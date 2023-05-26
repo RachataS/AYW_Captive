acb662ca21f71d9ba7438fb8e53eccdf64131f9b
RachataS <64030161@kmitl.ac.th>
2023-05-26 09:10:42 +0700

    add side bar

--- src/layouts/main-layout/MainLayout.vue
+++ src/layouts/main-layout/MainLayout.vue
@@ -4,6 +4,10 @@
     <!--begin::Page-->
     <div class="app-page flex-column flex-column-fluid" id="kt_app_page">
       <KTHeader />
+      <!--begin::Wrapper-->
+      <div class="app-wrapper flex-column flex-row-fluid" id="kt_app_wrapper">
+        <KTSidebar />
+        <!--begin::Main-->
         <div class="app-main flex-column flex-row-fluid" id="kt_app_main">
           <!--begin::Content wrapper-->
           <div class="d-flex flex-column flex-column-fluid">
@@ -13,13 +17,20 @@
             </div>
           </div>
           <!--end::Content wrapper-->
+          <!--<KTFooter />-->
         </div>
         <!--end:::Main-->
+      </div>
       <!--end::Wrapper-->
     </div>
     <!--end::Page-->
   </div>
   <!--end::App-->
+
+  <!--<KTDrawers />
+  <KTScrollTop />
+  <KTModals />
+  <KTCustomize />-->
 </template>
 
 <script lang="ts">
